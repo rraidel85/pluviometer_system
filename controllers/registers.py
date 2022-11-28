8bbc2834a9c4d1ae309ee2788a4f92631d3802ec
Raidel <rraidel85@gmail.com>
2022-11-28 00:45:42 -0800

    Fix registers_pluv.html bug on registers_pluv_api load

--- controllers/registers.py
+++ controllers/registers.py
@@ -14,6 +14,7 @@ def registers_form():
     form = SQLFORM(db.Registers)
     is_editing = False
     register_date = None
+    current_page = "registers_pluv" if request.vars.pluv_id else "registers_list"
 
     if request.args(0):
         register = db.Registers(request.args(0, cast=int))
@@ -21,16 +22,58 @@ def registers_form():
         register_date = register.register_date
         is_editing = True
 
+    if request.vars.pluv_id:
+        form.vars.id_pluviometer = request.vars.pluv_id
+
     if form.process().accepted:
         response.js = "jQuery('#registers_modal').modal('hide');showMyNotification('success', 'Operación realizada exitosamente');updateTable();"
     elif form.errors:
         plugin_toastr_message_config('error', T('Existen errores en el formulario'))
 
-    return dict(form=form, is_editing=is_editing, register_date=register_date)
+    return dict(form=form, is_editing=is_editing,
+                register_date=register_date, current_page=current_page)
 
 #-----------------------------------------------
 # APIS
 #-----------------------------------------------
+@request.restful()
+def registers_pluv_api():
+    """Return asynchronous registers data for Datatable based on a pluviometer id"""
+    response.view = 'generic.json'
+
+    def GET(*args, **kwargs):
+        start = int(request.vars.start)
+        limit = int(request.vars.limit)
+        search = request.vars.search
+        order_column = request.vars.order_column
+        order_dir = request.vars.order_dir
+        pluv_id = request.vars.pluv_id
+
+        registers = []
+        count = 0
+        try:
+            registers = db((db.Registers.id_pluviometer == pluv_id) &
+                              (db.Registers.register_date.like(f'{search}%'))).select(
+                                orderby=f'{db.Registers[order_column]} {order_dir}',
+                                limitby=(start,start+limit)).as_list()
+
+            count_query = db.Registers.id.count()
+            count = db((db.Registers.id_pluviometer == pluv_id) &
+                       (db.Registers.register_date.like(f'{search}%'))).select(count_query,
+                                                                               cache=(cache.ram, None),cacheable=True).first()[count_query]
+
+        except Exception as e:
+            print(e)
+
+        data = {
+            'length': count,
+            'registers': registers
+        }
+
+        return data
+
+    return locals()
+
 @request.restful()
 def registers_api():
     """Return asynchronous registers data for the Datatable"""
@@ -72,40 +115,3 @@ def registers_api():
     return locals()
 
 
-@request.restful()
-def registers_pluv_api():
-    """Return asynchronous registers data for Datatable based on a pluviometer id"""
-    response.view = 'generic.json'
-
-    def GET(*args, **kwargs):
-        start = int(request.vars.start)
-        limit = int(request.vars.limit)
-        search = request.vars.search
-        order_column = request.vars.order_column
-        order_dir = request.vars.order_dir
-        pluv_id = request.vars.pluv_id
-
-        registers = []
-        count = 0
-        try:
-            registers = db((db.Registers.id_pluviometer == pluv_id) &
-                              (db.Registers.register_date.like(f'{search}%'))).select(
-                                orderby=f'{db.Registers[order_column]} {order_dir}',
-                                limitby=(start,start+limit)).as_list()
-
-            count_query = db.Registers.id.count()
-            count = db((db.Registers.id_pluviometer == pluv_id) &
-                       (db.Registers.register_date.like(f'{search}%'))).select(count_query,
-                                                                               cache=(cache.ram, None),cacheable=True).first()[count_query]
-            print(count)
-        except Exception as e:
-            print(e)
-
-        data = {
-            'length': count,
-            'registers': registers
-        }
-
-        return data
-
-    return locals()
